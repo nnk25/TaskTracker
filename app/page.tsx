a7950600c3f7036cfec5c98abd0a45579ec4c9dc
nnk25 <2023cs088@stu.ucsc.cmb.ac.lk>
2026-05-28 01:08:44 +0530

    redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,5 @@
-import Link from "next/link";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  return (
-    <div>
-      <Link href="/tasks">Go to Tasks</Link>
-    </div>
-  );
+export default function Page() {
+  redirect("/tasks/");
 }
