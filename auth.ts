ab69a2134b7d93966b9a0b03c5725622ef2246e2
nnk25 <2023cs088@stu.ucsc.cmb.ac.lk>
2026-05-27 20:41:19 +0530

    prisma client added

--- auth.ts
+++ auth.ts
@@ -2,6 +2,16 @@ import NextAuth from "next-auth"
 import Credentials from "next-auth/providers/credentials"
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Credentials]
+  providers: [Credentials({
+    authorize: async (credentials) => {
+                if(!credentials?.username || !credentials?.password) return null
+                const {username, password} = credentials as {username: string, password: string}
+              
+                const user = await getUser(username)
+                console.log(user)
+                return user
+            }
+
+  })]
   
 })
